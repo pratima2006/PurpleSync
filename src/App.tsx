a08efb3bd81cacf3cc423faeb222e748649ccdd5
pratima2006 <pratimatribhuvan2006@gmail.com>
2026-09-24 20:43:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ function Shell() {
 
   return (
     <div className="ps-shell">
-      <main className="ps-main ml-0 md:ml-[258px]">
+      <main className="ps-main ml-0 md:ml-[110px]">
         <Topbar page={page} onNavigate={navigate} onSearch={() => setSearchOpen(true)} onMenu={() => setMobileMenuOpen(true)} />
         {content}
       </main>
